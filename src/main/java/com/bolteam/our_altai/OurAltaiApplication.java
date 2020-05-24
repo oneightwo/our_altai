b3f9605dd1ecdedc91afbe11bf24c0d32597b79e
oneightwo <oneightwo.company@gmail.com>
2020-05-24 17:01:24 +0400

    Initial commit

---
+++ src/main/java/com/bolteam/our_altai/OurAltaiApplication.java
@@ -0,0 +1,11 @@
+package com.bolteam.our_altai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurAltaiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OurAltaiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/bolteam/our_altai/OurAltaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolteam.our_altai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurAltaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
